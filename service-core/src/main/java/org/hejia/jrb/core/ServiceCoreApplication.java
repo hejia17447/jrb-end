caff0449d7dcf35ddf29e5e77a5e83f1a491a084
贺佳 <hejia1744709356@163.com>
2023-05-31 16:23:54 +0800

    添加配置，并测试是否启动成功

---
+++ service-core/src/main/java/org/hejia/jrb/core/ServiceCoreApplication.java
@@ -0,0 +1,15 @@
+package org.hejia.jrb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"org.hejia.jrb"})
+public class ServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+
+}
---
+++ service-core/src/main/java/org/hejia/jrb/core/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package org.hejia.jrb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("org.hejia.jrb.core.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
