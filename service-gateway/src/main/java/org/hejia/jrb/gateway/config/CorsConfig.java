f5c167895949856e034264bdd93c220b3b8cd3a0
贺佳 <hejia1744709356@163.com>
2023-07-05 10:26:37 +0800

    解决跨域问题

--- service-gateway/src/main/java/org/hejia/jrb/gateway/config/CorsConfig.java
+++ service-gateway/src/main/java/org/hejia/jrb/gateway/config/CorsConfig.java
@@ -1,25 +1,30 @@
 package org.hejia.jrb.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+@Slf4j
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsFilter() {
+        log.debug("CORS限制打开");
         CorsConfiguration config = new CorsConfiguration();
         // 是否允许携带cookie
         config.setAllowCredentials(true);
         // 是否允许携带cookie
-        config.addAllowedOrigin("*");
+//        config.addAllowedOrigin("*");
         // 允许携带的头
         config.addAllowedHeader("*");
         // 允许访问的方式
         config.addAllowedMethod("*");
+
+        config.addAllowedOriginPattern("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
